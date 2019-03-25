04b47f7ae3055eadb0304984748b5380b04471d0
Ponzi <43649035+Ponzis-Pitiful-Python@users.noreply.github.com>
2019-03-25 17:41:49 -0400

    Add files via upload

--- asciiOnlytest.py
+++ asciiOnlytest.py
@@ -2,7 +2,6 @@
 #https://stackoverflow.com/questions/35889505/check-that-a-string-contains-only-ascii-characters/35895857
 daString = input("Paste string here: ")
 
-
 if all(ord(char) < 128 for char in daString):
     print("All characters are ascii")
 else:
